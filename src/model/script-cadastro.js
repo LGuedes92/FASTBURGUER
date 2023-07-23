4c11f60b4ab7c5d99e197e233d73de6904581844
LGuedes92 <lbguedes@sga.pucminas.br>
2023-07-23 12:35:42 -0300

    Update script-cadastro.js

--- src/model/script-cadastro.js
+++ src/model/script-cadastro.js
@@ -145,8 +145,7 @@ function gerarCartoesProdutos(categoriaSelecionada) {
 // Chamar a função para gerar os cartões de produto com base nos dados do LocalStorage e categoria inicial
 var categoriaSelecionada = document.getElementById('categ').value;
 document.addEventListener('DOMContentLoaded', function() {
-  gerarCardsProdutos('hamburguers');
-  gerarCardsProdutos('bebidas');
-  gerarCartoesPromocao();
+  gerarCartoesProdutos('hamburguers');
+  gerarCartoesProdutos('bebidas');
 });
 
